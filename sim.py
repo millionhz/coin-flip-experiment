cc83aa838541a1230aa7131544fcf138f48a130a
Hamza <m.waqas.hamza@gmail.com>
2020-06-09 16:23:27 +0500

    deque added; statemt prints added

--- sim.py
+++ sim.py
@@ -1,26 +1,49 @@
 from random import randint
+from collections import deque
 
+print('Initializing....')
 subjects = ({'id': randint(100000, 999999), 'flips': []}
-            for x in range(10000))
-standing = [subject for subject in subjects]
-sitting = []
-
+            for x in range(100000))
+standing = deque([subject for subject in subjects])
+sitting = deque([])
+round = 0
+print('Initialized')
 
 def sit_stand(standing):
-    dic = standing.copy()
+    global subjects_lost
+    subjects_lost = 0
+    dic = iter(standing.copy())
     for x in dic:
         if 0 in x['flips'][:-1]:
             print('ANOMALY')
         if x.get('flips')[-1] == 0:
+            #print(f'{x["id"]} lost')
+            subjects_lost += 1
             sitting.append(x)
             standing.remove(x)
 
 
-while len(standing) != 1:
+while len(standing) > 1:
+    round += 1
     for x in standing:
+        #print(f'Processing {x.get("id")}...')
         x['flips'].append(randint(0, 1))
+    print('Coins Fliped!')
+    r1p = len(standing)
     sit_stand(standing)
-print(sitting[-2])
-print(sitting[-1])
+    print(f'Round {round} over!')
+    print(f'Out of {r1p}; {subjects_lost} subjects lost in this round...')
+    try:
+        print(f'Loss Ratio {(subjects_lost/r1p *100)}%\n')
+    except:
+        break
+
+#print()
+#print(sitting)
 print()
+standing = list(standing)
 print(standing)
+try:
+    print(f'{standing[-1]["id"]} fliped heads {len(standing[-1]["flips"])} times \n')
+except:
+    pass
